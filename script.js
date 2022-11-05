d1f7ae9202ef7b23de42137e03d41ac909a36e17
MrBurp <andreass.ziegler@gmail.com>
2022-11-05 11:02:30 +0100

    fn upper, lower and number

---
+++ script.js
@@ -0,0 +1,13 @@
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber());
