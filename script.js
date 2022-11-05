04d7ed7ac44230c3099b6de17b048f29445d80d7
MrBurp <andreass.ziegler@gmail.com>
2022-11-05 11:14:15 +0100

    fn symbol

--- script.js
+++ script.js
@@ -10,4 +10,7 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-console.log(getRandomNumber());
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
