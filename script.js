b0a11bd98d6f6b33537013e6f7acb2c3d560812e
MrBurp <andreass.ziegler@gmail.com>
2022-11-07 17:02:33 +0100

    generatePassword working

--- script.js
+++ script.js
@@ -29,6 +29,25 @@ generateEl.addEventListener("click", () => {
   );
 });
 
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) {
+    return "";
+  }
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+}
+
 function getRandomLowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
